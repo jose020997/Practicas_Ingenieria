d26b80e477d62fa82b9a6d4c31121084911ffcf9
josea <josea@DESKTOP-OOST17O>
2023-02-11 14:19:34 +0100

    Commit primero

---
+++ demo/src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+// error en la sintaxis, click izquierdo para las sugerencias (En este caso era el Import)
+@Controller
+public class HomeController {
+
+	@GetMapping(value="/inicio")
+	public String metodoinicio() {
+		return "index";
+	}
+// Se guardan los cambios
+}
