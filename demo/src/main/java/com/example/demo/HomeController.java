9936b0b76271ca7a27aacba5fff13edd2f162fe9
josea <josea@DESKTOP-OOST17O>
2023-02-25 13:33:04 +0100

    No entra pero lo hacemos en bloque, no lee el usuario

--- demo/src/main/java/com/example/demo/HomeController.java
+++ demo/src/main/java/com/example/demo/HomeController.java
@@ -18,19 +18,40 @@ public class HomeController {
 	}
 	
 	@GetMapping(value="/datos")
-	public String devolverdatos() {
+	public String verUsuario(Model model,HttpSession sesion,HttpServletRequest request) {
+		HttpSession session = request.getSession(true);
+		boolean mensaje;
+		Usuario user = (Usuario) sesion.getAttribute("usuario");
+		 
+		if(user != null) {
+			model.addAttribute("usuario", user);
+		 	System.out.println(model);
+		 	mensaje=false;
+		}
+		else {
+			mensaje=true;
+		}
 		return "datos";
 	}
 	
 	@PostMapping(value="/datos")
 	public String metodo(HttpServletRequest req, Model model) {
 		HttpSession sesion = req.getSession();
-		//Se hace el request que es el que responde a las sesiones
-		String email = req.getParameter("email");
-		String nombre = req.getParameter("nombre");
-		//	System.out.println(email+" "+nombre);
+		//Se hace el request que es el que responde a las sesiones cookies
+		
+			String email = req.getParameter("email");
+			String nombre = req.getParameter("nombre");
+			//Guardamos los datos en un string que rescatamos de la sesion http
+			
 		Usuario usuario = new Usuario(nombre,email);
-		model.addAttribute("usuario", usuario);
+		//Creamos un javabean de usuario con los datos rescatados de la sesion
+		System.out.println(nombre);
+		System.out.println(email);
+		System.out.println(usuario);
+		
+		sesion.setAttribute("usuario", usuario);
+		//guardamos en la sesion el datos usuario
+		System.out.println(sesion);
 		return "datos";
 		
 	}
