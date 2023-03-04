f819089f9b566f47cfa69012146c681716bf019b
josea <josea@DESKTOP-OOST17O.mshome.net>
2023-03-04 18:03:34 +0100

    Principio del formulario

--- demo/src/main/java/com/example/demo/HomeController_jabg0014.java
+++ demo/src/main/java/com/example/demo/HomeController_jabg0014.java
@@ -1,8 +1,13 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController_jabg0014 {
 	
+	@GetMapping("/login")
+    public String mostrarlogin() {
+		return "login";
+    }
 }
